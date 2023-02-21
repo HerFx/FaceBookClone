c4daacd1c35b08e2ec2dd6f38cc5773395629a96
Mikołaj Janiak <janiak.m2222@gmail.com>
2023-02-21 13:35:47 +0100

    Posts

--- fbclone/src/components/main_content/main_input_post.js
+++ fbclone/src/components/main_content/main_input_post.js
@@ -4,17 +4,41 @@ import {useState} from 'react';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import Main_post from './Main_post';
 
 export default function Main_input_post() {
   const [input, setInput] = useState('')
-  return (
-
+  const [posts, setPosts] = useState([])
+  const [file, setFile] = useState()
+  const [fileUrl, setFileUrl] = useState()
   
-  <div className='input__post_box'>
+  const onChange = (e) => {
+    setInput(e.target.value)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setPosts([...posts, input])
+        setInput('')
+    }
+
+    const onFileChange = (e) => {
+       setFile(e.target.files[0])
+       const reader = new FileReader()
+         reader.onloadend = () => {
+              setFileUrl(reader.result)
+            }
+            reader.readAsDataURL(e.target.files[0])
+    }
+
+
+  return (
+<>
+<div className='input__post_box'>
     <div className='input__post'>
         <Avatar src={null} className='input__avatar'/>
-        <form>
-            <input type='text' placeholder='What is on your mind?' />
+        <form onSubmit={onSubmit}>
+            <input value={input} type='text' placeholder='What is on your mind?' onChange={onChange}/>
         </form>
     </div>
     <div className='input__post_options'>
@@ -23,8 +47,9 @@ export default function Main_input_post() {
             <h4>Live Video</h4>
         </div>
         <div className='input__post_option'>
-            <PhotoLibraryIcon style={{color: 'green'}} />
+            <PhotoLibraryIcon style={{color: 'green'}} on/>
             <h4>Photo/Video</h4>
+            <input className='photo_input' type='file'onChange={onFileChange}></input>
         </div>
         <div className='input__post_option'>
             <InsertEmoticonIcon style={{color: 'orange'}} />
@@ -32,5 +57,10 @@ export default function Main_input_post() {
         </div>
     </div>
   </div>
+    {posts.map((post, image) => (
+        <Main_post post={post} image={fileUrl}/>
+    ))}
+</>
+  
   )
 }
--- fbclone/src/components/main_content/main_post.js
+++ fbclone/src/components/main_content/main_post.js
@@ -1,32 +1,48 @@
 import React from 'react'
+import {useState} from 'react';
+import {useEffect} from 'react';
+import {Avatar}  from '@mui/material';
 
-export default function Main_post({avatar, name, time, post, like, comment, share}) {
+
+
+
+export default function Main_post({avatar, post, image}) {
+    const [time, setTime] = useState('')
+    const [likes, setLikes] = useState(0)
+
+    const like = () => {
+        setLikes(likes + 1)
+    }
+    
+
+    useEffect(() => {
+        setTime(new Date().toLocaleTimeString())
+    }, [])
+
   return (
     <div>
         <div className="main_post">
             <div className="main_post_header">
                 <div className="main_post_header_left">
-                    <img src={avatar} alt="avatar" />
+                    <Avatar src={null}/>
                     <div className="main_post_header_left_name">
-                        <h4>{name}</h4>
+                        <h4>FirsName LastName</h4>
                         <p>{time}</p>
                     </div>
-                </div>
-                <div className="main_post_header_right">
-                    <i className="fas fa-ellipsis-h"></i>
                 </div>
             </div>
             <div className="main_post_body">
                 <p>{post}</p>
+                <img src={image}></img>
+                <div className="main_post_likes">
+                    {likes > 0 ? <p><i className="far fa-thumbs-up"></i>{likes}</p> : null}
+                </div>
             </div>
             <div className="main_post_footer">
                 <div className="main_post_footer_left">
-                    <i className="far fa-thumbs-up"></i>
-                    <i className="far fa-comment"></i>
-                    <i className="fas fa-share"></i>
-                </div>
-                <div className="main_post_footer_right">
-                    <i className="far fa-bookmark"></i>
+                    <button onClick={like}><i className="far fa-thumbs-up"></i>Like!</button>
+                    <button><i className="far fa-comment"></i>Comment</button>
+                    <button><i className="fas fa-share"></i>Share</button>
                 </div>
             </div>
         </div>
